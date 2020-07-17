d3920414db8f063c01abd4f11768f6fa74f5be5a
ChongLynk <tronglinhmta0611@gmail.com>
2020-07-17 00:47:48 -0400

    add transaction and completed

--- routes/transaction.router.js
+++ routes/transaction.router.js
@@ -4,12 +4,16 @@ var router = express.Router();
 var db = require('../db');
 
 router.get('/' , (req, res)=>{
-    res.render('transaction/transaction',{
-
-    });
+    res.render('transaction/transaction');
 })
 router.get('/create',(req,res)=>{
     res.render('transaction/create');
+})
+
+
+router.post('/' , (req ,res)=>{
+    db.get('transaction').push(req.body).write();
+    res.redirect('/transaction');
 })
 
 module.exports = router;
